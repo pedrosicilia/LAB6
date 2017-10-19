f70daa47a5c30ca11ba454817e18e4a915fefd0a
Pedrosicilia <pedro@siblaz.com>
2017-10-19 01:55:42 -0400

    COMPLETELY DONE

--- src/Main.java
+++ src/Main.java
@@ -1,65 +1,59 @@
 public class Main {
 
 
-    public static int[][] filldown(int[][] filldownarray, int stepsize) {
+    public static int[][] fillDown(int[][] fillDownArray, int stepsize) {
         int row0 = 0;
         int col0 = 0;
         int stepnum = 1;
 
-
-        filldownarray[row0][col0] = stepsize;
-
-
-        // System.out.println( "FIRST: " + filldownarray[row0][col0] + "ROW LENGTH: " + filldownarray.length + " COL LENGTH: " +  filldownarray[0].length);
+        fillDownArray[row0][col0] = stepsize;
         col0 = -1;
-        while (col0 < filldownarray[0].length) {
+        while (col0 < fillDownArray[0].length) {
             row0 = 0;
             col0++;
-            System.out.println("NEW COL: " +col0);
+          //  System.out.println("NEW COL: " +col0);
 
-            if(col0 == filldownarray[0].length){
+            if(col0 == fillDownArray[0].length){
 
-                System.out.println( "COORD; 2,4 " + filldownarray[1][1]);
+        //        System.out.println( "COORD; 2,4 " + fillDownArray[1][1]);
 
-                return filldownarray;
+                return fillDownArray;
             }
 
 
-            while (row0 < filldownarray.length) {
-                filldownarray[row0][col0] = stepsize * stepnum;
-                System.out.println("STEP:" + stepnum + " NUm: " + filldownarray[row0][col0] + " Row: " + row0);
+            while (row0 < fillDownArray.length) {
+                fillDownArray[row0][col0] = stepsize * stepnum;
+            //    System.out.println("STEP:" + stepnum + " NUm: " + fillDownArray[row0][col0] + " Row: " + row0);
                 stepnum++;
                 row0++;
             }
         }
 
 
-        return filldownarray;
+        return fillDownArray;
 
     }
 
 
-    public static int[][] fillright(int[][] fillrightarray, int stepsize) {
+    public static int[][] fillRight(int[][] fillRightArray, int stepsize) {
 
         int row0 = 0;
         int col0 = 0;
         int stepnum = 1;
 
-        fillrightarray[row0][col0] = stepsize;
-        System.out.println( "STEPSIZE: " +stepsize+ " NUM: " +fillrightarray[row0][col0]);
+        fillRightArray[row0][col0] = stepsize;
+     //   System.out.println( "STEPSIZE: " +stepsize+ " NUM: " +fillRightArray[row0][col0]);
 
         row0 = -1;
-        while (row0 < fillrightarray.length) {
+        while (row0 < fillRightArray.length) {
             col0 = 0;
             row0++;
 
 
-            System.out.println("NEW ROW: " +row0);
+         //   System.out.println("NEW ROW: " +row0);
 
-            if(row0 == fillrightarray.length){
-                System.out.println( "COORD; 2,4 " + fillrightarray[0][1]);
-
-                return fillrightarray;
+            if(row0 == fillRightArray.length){
+                return fillRightArray;
             }
 
 
@@ -67,76 +61,19 @@ public class Main {
 
 
 
-            while (col0 < fillrightarray[0].length) {
-                fillrightarray[row0][col0] = stepsize * stepnum;
-                System.out.println("STEP:" + stepnum + " NUm: " + fillrightarray[row0][col0] + " Col: " +col0);
+            while (col0 < fillRightArray[0].length) {
+                fillRightArray[row0][col0] = stepsize * stepnum;
+             //   System.out.println("STEP:" + stepnum + " NUm: " + fillRightArray[row0][col0] + " Col: " +col0);
                 stepnum++;
                 col0++;
             }
         }
 
-            System.out.println( "COORD; 2,4 " + fillrightarray[2][4]);
+           // System.out.println( "COORD; 2,4 " + fillRightArray[2][4]);
 
-        return fillrightarray;
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-        public static void printarray(int[][] printedarray) {
-
-        int col = 0;
-        int row = 0;
-
-        for (int i = 0; i < printedarray.length; i++) {
-            for (int j = 0; j < printedarray[i].length; j++) {
-                System.out.format("%-10d",	printedarray[i][j]);
-            }
-            System.out.println();
-        }
-
-
-        /*
-
-        System.out.println(" COL length: " +printedarray[0].length);
-
-        while(col <= printedarray[0].length) {
-
-            System.out.println("TESTes");
-            System.out.format("%-10d",	printedarray[row][col]);
-            row++;
-
-
-            if(printedarray.length == row){
-                row = 0;
-                col++;
-            }
-
-        }
-
-
-        return;
+        return fillRightArray;
 
     }
-
-                */
-
 
 
 
@@ -151,7 +88,7 @@ public class Main {
             col0++;
         }
 
-        System.out.println( "SUM OF ROW 0: " +sum);
+       // System.out.println( "SUM OF ROW 0: " +sum);
 
         return sum;
     }
@@ -168,103 +105,77 @@ public class Main {
             row0++;
         }
 
-        System.out.println( "SUM OF col 0: " +sum);
+      //  System.out.println( "SUM OF col 0: " +sum);
 
         return sum;
-
-
-
-
     }
+
+
+
+        public static int[][] printarray(int[][] printedarray) {
+
+            int col0 = 0;
+            int row0 = 0;
+            boolean qwe = true;
+
+            while (row0 < printedarray.length) {
+                col0 = 0;
+                row0++;
+                if (row0 == printedarray.length) {
+                    return printedarray;
+                }
+                while (col0 < printedarray[0].length) {
+
+                    if(qwe) {
+                        row0 = 0;
+                        qwe = false;
+                    }
+                    System.out.format("%-10d", printedarray[row0][col0]);
+                    col0++;
+                }
+                System.out.println();
+
+            }
+                    return printedarray;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 
         public static void main(String args[]) {
-        System.out.println("TEST");
-        int[][] filldownarray = new int[5][8];
-
-        System.out.println("TEST 120");
 
 
-        filldown(filldownarray, -2);
+            int[][] fillRightArray = new int[5][8];
+            fillRight(fillRightArray, 2);
+            printarray(fillRightArray);
+            System.out.println();
 
-        int[][] fillrightarray = new int[5][8];
+            int[][] fillDownArray = new int[5][8];
+            fillDown(fillDownArray, -2);
+            printarray(fillDownArray);
+            System.out.println();
 
-        System.out.println("TEST 124");
+            System.out.println(getRowSum(fillRightArray, 0));
+            System.out.println(getColSum(fillDownArray, 0));
 
-        fillright(fillrightarray, 2);
-
-
-            getRowSum(fillrightarray, 0);
-            getColSum(filldownarray, 0);
-
-        System.out.println("TEST 126");
-        //  printarray(filldownarray);
-        System.out.println();
     }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- /*
-
-
-         int row = 0;
-        int col = 0;
-        boolean qwe = true;
-        int rowminus = 1;
-
-        filldownarray[row][col] = stepsize;
-        System.out.println("ROW LENGTH: " + filldownarray.length + " COL LENGTH: " + filldownarray[0].length);
-
-       // for(int count = 0; count < filldownarray.length; count++) {
-        System.out.println( " TEST: " + filldownarray[0][0]);
-
-        /*
-        for(int col = 0; col < filldownarray[0].length; col++) {
-            for(int row = 1; row < filldownarray.length; row++) {
-
-                filldownarray[row][col] = filldownarray[row][col] + stepsize;
-
-            }
-
-
-            while(col < filldownarray.length) {
-
-        filldownarray[row][col] = filldownarray[row][col] + 2;
-        System.out.println( "ROW: " + row + " Col: " + col + " is " + filldownarray[row][col]);
-        row++;
-
-
-        if (row == filldownarray.length) {
-        // count = 0;
-        row = 0;
-        col++;
-
-        if(col == filldownarray[0].length) {
-        break;
-        }
-
-        }
-
-        }
-
-
-
-
-
-  */
